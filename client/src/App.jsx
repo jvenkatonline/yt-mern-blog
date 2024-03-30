2673faddfadad5faaaa41bb4073ea66ec459fe14
softservs <jvenkatonline@gmail.com>
2024-03-30 15:55:11 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import About from "./assets/pages/About";
+import Signin from "./assets/pages/Signin";
+import SignUp from "./assets/pages/SignUp";
+import Dashboard from "./assets/pages/Dashboard";
+import Projects from "./assets/pages/Projects";
 const App = () => {
-  return <div className="text-xl text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
